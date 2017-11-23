0091e7517170eda152ed47952310841592382609
eugene <eugene.it.start@gmail.com>
2017-11-23 11:43:29 +0200

    test

--- resources/views/blog/detail.blade.php
+++ resources/views/blog/detail.blade.php
@@ -12,7 +12,14 @@
                     <br>
                     <small>{{$article->created_at}}</small>
                 </h2>
+
                 <hr>
+
+                <button id="search">Search image</button>
+                <div id="img">
+                    <img src="" alt="">
+                </div>
+                <button id="insert">Insert image</button>
                 <img alt="" src="" id="imgInArticle">
                 {{$article->content}}
                 <hr>
